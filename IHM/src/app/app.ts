51e7cc1c7ab64e7c61eb979176feb51bd6441fc7
BonjourPointDoc <valentinesoddu86@gmail.com>
2025-06-12 14:11:28 +0200

    Fix folders

--- IHM/src/app/app.ts
+++ IHM/src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `<h1>Fruit juice</h1>`,
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'froot';
+  protected title = 'IHM';
 }
