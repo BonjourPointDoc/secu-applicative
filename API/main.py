c41290ab700d383b78a378abe07d4e778cc479cb
Romain Carlino <romain.carlino@etu.univ-grenoble-alpes.fr>
2025-06-12 19:27:36 +0200

    Added logging to API

--- API/main.py
+++ API/main.py
@@ -1,6 +1,25 @@
-from fastapi import FastAPI
+import logging
 
+from fastapi import FastAPI
+from logging import basicConfig, INFO, getLogger, DEBUG, ERROR
+
+# Parameters TODO get parameters from a config file
+APP_debugging = True
+
+# Initialise API
 app = FastAPI()
+
+#Initialise logger
+basicConfig(
+    format="%(levelname)s (APP): %(message)s",
+    level=INFO
+)
+logger = getLogger(__name__)
+
+if APP_debugging:
+    logger.setLevel(level=DEBUG)
+else:
+    logger.setLevel(level=ERROR)  # For production mode logger is set to ERROR
 
 @app.get("/")
 async def root():
