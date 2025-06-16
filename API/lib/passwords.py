c0f8f8c9bd25e3e92d16329ebb864fe2224ccbcf
Romain Carlino <romain.carlino@etu.univ-grenoble-alpes.fr>
2025-06-16 19:29:41 +0200

    Added function to verify input password (without db connection for now)

--- API/lib/passwords.py
+++ API/lib/passwords.py
@@ -1,6 +1,7 @@
-from common import logger
+from .common import logger
 from bcrypt import gensalt, hashpw, checkpw
 from datetime import datetime
+from time import sleep
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode('utf-8')
@@ -11,7 +12,19 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, password_hash: str) -> bool:
     res = checkpw(password.encode('utf-8'), password_hash.encode('utf-8'))
-    if not res:
+    if not res or password == "fail": # Second conditions added for tests
         logger.error(f"Password error at {datetime.now()}")
+        sleep(2) # Adding timer to avoid brute force attacks
+        return False
 
     return res
+
+def verify_credentials(login: str, password: str) -> bool:
+
+    # Dummy code waiting for real db access
+    logger.error(f"WIP function no real db connection : {login}")
+    password_hash: str = hash_password(password)
+    if verify_password(password, password_hash): # If this bug very huge bug /!\
+        return True
+    else:
+        return False
--- API/main.py
+++ API/main.py
@@ -1,10 +1,8 @@
-from inspect import stack
-
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
-from lib.common import logger
 from lib.models import Status, StatusOutput ,LoginInput, LoginOutput
+from lib.passwords import *
 
 # Initialise API
 app = FastAPI(title="Juice shop", version="beta")
@@ -19,10 +17,11 @@ async def root():
           responses={401: {"model": LoginOutput, "description": "Wrong credentials"}})
 async def login(json_input: LoginInput):
     # WIP in progress to test framework
-    if json_input.login == "fail":  # Used ti simulate wrong credentials
+
+    if not verify_credentials(json_input.login, json_input.password):  # Used ti simulate wrong credentials
         return JSONResponse(status_code=401,
                             content=StatusOutput(status= Status.ERROR,
-                                                 msg= "Wrong crendentials").model_dump())
+                                                 msg= "Wrong credentials").model_dump())
     output = LoginOutput( # Using LoginOutput model to assert server is sending normal output
         status= Status.WIP,
         api_key= f"bidon key for {json_input.login} + {json_input.password}")
